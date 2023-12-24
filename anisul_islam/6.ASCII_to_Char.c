1488fa9d69ae59faa248aa3e21615d139a309e8e
aqShanto <abdulquymshanto@gmail.com>
2023-12-24 15:51:33 +0600

    comvert ASCII to Char

---
+++ anisul_islam/6.ASCII_to_Char.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    char ASCII;
+    char Character;
+    printf("Enter your ASCII value:");
+    scanf("%d", &ASCII);
+    printf("Your Character is: %c", ASCII);
+    return 0;
+}
