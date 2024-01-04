2b15974e01f4223859dfe4bff8ae8a01bb75eb70
aqShanto <abdulquymshanto@gmail.com>
2024-01-04 18:01:31 +0600

    update

--- PlayingWithNumbers.c
+++ PlayingWithNumbers.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int bin =0, rem =0, place =1,count=0;
+    int bin =0, rem =0, place =1,q=0;
     int num;
     printf("Enter your Query number:");
     scanf("%d",&num);
@@ -11,38 +11,45 @@ int main(){
         num     = num/2;
         bin     = bin+(rem*place);
         place   = place*10;
-        count   = count+rem;
+        q   = q+rem;
     }
 
-    for(int i=0;i<=32-count; i++){
+    for(int i=0;i<=32-q; i++){
         printf("%d", 0);
     }
     printf("%d\n", bin);
-    printf("Number of 1s is %d\n", count);
+    printf("Number of 1s is %d\n", q);
 
     int n;
     printf("Number of Candidate:");
     scanf("%d",&n);
 
+    int maximum, minimum, diff;
     for(int i=0; i<n; i++){
     printf("Enter your Candidates number:");
     scanf("%d",&num);
-    int bin =0, rem =0, place =1,count=0;
+    int bin =0, rem =0, place =1,c=0;
     while (num!=0)
     {
         rem     = num%2;
         num     = num/2;
         bin     = bin+(rem*place);
         place   = place*10;
-        count   = count+rem;
+        c   = c+rem;
     }
-    for(int i=0;i<=32-count; i++){
+    for(int i=0;i<=32-c; i++){
         printf("%d", 0);
     }
     printf("%d\n", bin);
-    printf("Number of 1s is %d\n", count);
+    printf("Number of 1s is %d\n", c);
+    diff = q-c;
+    if(c>q){
+        maximum=c;
     }
-
-
+    if(c<q){
+        minimum=c;
+    }
+    }
+    printf("Number with Minimum %d", minimum);
     return 0;
 }
