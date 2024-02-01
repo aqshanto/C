0aac0c4c62223a1ca7105519d5e2c99ebcd0928f
aqShanto <abdulquymshanto@gmail.com>
2024-02-02 02:56:47 +0600

    solving 2755

--- BeeCrowd/2755.c
+++ BeeCrowd/2755.c
@@ -4,6 +4,6 @@ int main(){
     printf("\"Ro'b'er\tto\\/\"\n");
     printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
     printf("(^_-) (-_-) (-_^)\n");
-    printf("("_") ('.')\n");
+    printf("(\"_\") ('.')\n");
     return 0;
 }
