9c132b34c2157591e10be684f7558c2119bcc09e
aqShanto <abdulquymshanto@gmail.com>
2024-02-06 18:27:30 +0600

    solving 1021

--- BeeCrowd/1021.c
+++ BeeCrowd/1021.c
@@ -5,19 +5,20 @@ int main(){
     float money , extra;
     scanf("%f", &money);
     int a, b, c, d, e, f ;
-
-    a = money / 100;
-    extra = fmod(money,100);
-    b = extra / 50;
-    extra = fmod(extra, 50);
-    c = extra / 20;
-    extra = fmod(extra, 20);
-    d = extra / 10;
-    extra = fmod(extra, 10);
-    e = extra / 5;
-    extra = fmod(extra , 5);
-    f = extra / 2;
-    extra = fmod(extra , 2);
+    
+    money = money * 100;
+    a = money / 10000;
+    extra = fmod(money,10000);
+    b = extra / 5000;
+    extra = fmod(extra, 5000);
+    c = extra / 2000;
+    extra = fmod(extra, 2000);
+    d = extra / 1000;
+    extra = fmod(extra, 1000);
+    e = extra / 500;
+    extra = fmod(extra , 500);
+    f = extra / 200;
+    extra = fmod(extra , 200);
 
     printf("NOTAS:\n");
     printf("%d nota(s) de R$ 100.00\n", a);
@@ -28,25 +29,25 @@ int main(){
     printf("%d nota(s) de R$ 2.00\n", f);
 
     int g , h , i , j , k ,l;
-    g = extra /1;
-    extra = fmod(extra, 1);
-    h = extra /0.50;
-    extra = fmod(extra,0.50);
-    i = extra /0.25;
-    extra = fmod(extra, 0.25);
-    j = extra /0.10;
-    extra = fmod(extra, 0.10);
-    k = extra /0.05;
-    extra = fmod(extra, 0.05);
-    l = extra /0.01;
+    g = extra /100;
+    extra = fmod(extra, 100);
+    h = extra /50;
+    extra = fmod(extra,50);
+    i = extra /25;
+    extra = fmod(extra, 25);
+    j = extra /10;
+    extra = fmod(extra, 10);
+    k = extra /5;
+    extra = fmod(extra, 5);
+    l = extra /1;
 
     printf("MOEDAS:\n");
-    printf("%d nota(s) de R$ 1.00\n", g);
-    printf("%d nota(s) de R$ 0.50\n", h);
-    printf("%d nota(s) de R$ 0.25\n", i);
-    printf("%d nota(s) de R$ 0.10\n", j);
-    printf("%d nota(s) de R$ 0.05\n", k);
-    printf("%d nota(s) de R$ 0.01\n", l);
+    printf("%d moeda(s) de R$ 1.00\n", g);
+    printf("%d moeda(s) de R$ 0.50\n", h);
+    printf("%d moeda(s) de R$ 0.25\n", i);
+    printf("%d moeda(s) de R$ 0.10\n", j);
+    printf("%d moeda(s) de R$ 0.05\n", k);
+    printf("%d moeda(s) de R$ 0.01\n", l);
 
     return 0;
 }
