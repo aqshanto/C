aa931bc56290638dd793a3cfe4e7aefb42430942
aqShanto <abdulquymshanto@gmail.com>
2024-02-07 01:04:49 +0600

    solving 1144

---
+++ BeeCrowd/1098.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    printf("I=0 J=1\n");
+    printf("I=0 J=2\n");
+    printf("I=0 J=3\n");
+    printf("I=0.2 J=1.2\n");
+    printf("I=0.2 J=2.2\n");
+    printf("I=0.2 J=3.2\n");
+    printf(".....\n");
+    printf("I=2 J=?\n");
+    printf("I=2 J=?\n");
+    printf("I=2 J=?\n");
+    
+}
---
+++ BeeCrowd/1133.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    int X,Y;
+    scanf("%d %d", &X,&Y);
+    if(X>0 &&Y>0)
+    {
+        if(X<Y)
+        {
+            for(int i=X;i<Y;i++)
+            {
+                if(i%5==2 || i%5==3)
+                {
+                    printf("%d\n",i);
+                }
+            }
+        }
+        else if(X>Y)
+        {
+            for(int i=Y;i<X;i++)
+            {
+                if(i%5==2 || i%5==3)
+                {
+                    printf("%d\n",i);
+                }
+            }
+        }
+    }
+    
+}
