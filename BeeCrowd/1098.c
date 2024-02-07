dbfbb1296a215cab3e23ff011a9b35e5babc1883
aqShanto <abdulquymshanto@gmail.com>
2024-02-08 01:06:44 +0600

    solving 1098

--- BeeCrowd/1098.c
+++ BeeCrowd/1098.c
@@ -1,15 +1,27 @@
 #include<stdio.h>
 
 int main(){
-    printf("I=0 J=1\n");
-    printf("I=0 J=2\n");
-    printf("I=0 J=3\n");
-    printf("I=0.2 J=1.2\n");
-    printf("I=0.2 J=2.2\n");
-    printf("I=0.2 J=3.2\n");
-    printf(".....\n");
-    printf("I=2 J=?\n");
-    printf("I=2 J=?\n");
-    printf("I=2 J=?\n");
+    float a ;
+    float b = 0 ;
+    float c = 0 ;
+    float d = 0 ;
+    a = 10;
+
+    for (int i =0 ; i<=a; i++){
+        int e ;
+        e = b *10;
+        if(e%10 ==0 ){
+            printf("I=%0.0f J=%0.0f\n", b , b+1);
+            printf("I=%0.0f J=%0.0f\n", c , c+2);
+            printf("I=%0.0f J=%0.0f\n", d , d+3);
+        }else{
+            printf("I=%0.1f J=%0.1f\n", b , b+1);
+            printf("I=%0.1f J=%0.1f\n", c , c+2);
+            printf("I=%0.1f J=%0.1f\n", d , d+3);
+        }
+        b = b + 0.2;
+        c = c + 0.2;
+        d = d + 0.2;
+    }
     return 0;
 }
