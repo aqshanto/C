5aa841854f2c2fc49da535f21e8c77e6eb4aacae
aqShanto <abdulquymshanto@gmail.com>
2024-02-14 12:21:02 +0600

    update 1045

--- BeeCrowd/1045.c
+++ BeeCrowd/1045.c
@@ -2,98 +2,50 @@
 #include<math.h>
 
 int main(){
-    double a,b,c;
-    scanf("%lf %lf %lf", &a ,&b ,&c);
+    double a,b,c,a1,b1,c1;
 
-    if(a>=b+c){
+    scanf("%lf %lf %lf", &a1 ,&b1 ,&c1);
+    if(a1>b1 && a1>c1){
+        a = a1;
+        if(b1>c1){
+            b =b1;
+            c =c1;
+        }else{
+            b =c1;
+            c =b1;
+        }
+    }
+    if(b1>a1 && b1>c1){
+        a = b1;
+        if(a1>c1){
+            b =a1;
+            c =c1;
+        }else{
+            b =c1;
+            c =a1;
+        }
+    }
+    if(c1>b1 && c1>a1){
+        a = c1;
+        if(b1>a1){
+            b =b1;
+            c =a1;
+        }else{
+            b =a1;
+            c =b1;
+        }
+    }
+    if(a>=b+c)
         printf("NAO FORMA TRIANGULO\n");
-
-        if(pow(a,2)==pow(b,2)+pow(c,2)){
+    if(pow(a,2)==pow(b,2)+pow(c,2))
             printf("TRIANGULO RETANGULO\n");
-        }else if(pow(a,2)>pow(b,2)+pow(c,2)){
+    if(pow(a,2)>pow(b,2)+pow(c,2))
             printf("TRIANGULO OBTUSANGULO\n");
-        }else if(pow(a,2)<pow(b,2)+pow(c,2)){
+    if(pow(a,2)<pow(b,2)+pow(c,2))
             printf("TRIANGULO ACUTANGULO\n");
-        }else if(a==b && b==c){
+    if(a==b && b==c)
             printf("TRIANGULO EQUILATERO\n");
-        }else if(a==b || b==c || a==c){
+    if(a==b || b==c || a==c)
             printf("TRIANGULO ISOSCELES\n");
-        }
-    }
-    else if(pow(a,2)==pow(b,2)+pow(c,2)){
-        printf("TRIANGULO RETANGULO\n");
-        
-        if(a>=b+c){
-        printf("NAO FORMA TRIANGULO\n");
-        }else if(pow(a,2)>pow(b,2)+pow(c,2)){
-            printf("TRIANGULO OBTUSANGULO\n");
-        }else if(pow(a,2)<pow(b,2)+pow(c,2)){
-            printf("TRIANGULO ACUTANGULO\n");
-        }else if(a==b && b==c){
-            printf("TRIANGULO EQUILATERO\n");
-        }else if(a==b || b==c || a==c){
-            printf("TRIANGULO ISOSCELES\n");
-        }
-    }
-    else if(pow(a,2)>pow(b,2)+pow(c,2)){
-        printf("TRIANGULO OBTUSANGULO\n");
-
-        if(a>=b+c){
-        printf("NAO FORMA TRIANGULO\n");
-        }else if(pow(a,2)==pow(b,2)+pow(c,2)){
-            printf("TRIANGULO RETANGULO\n");
-        }else if(pow(a,2)<pow(b,2)+pow(c,2)){
-            printf("TRIANGULO ACUTANGULO\n");
-        }else if(a==b && b==c){
-            printf("TRIANGULO EQUILATERO\n");
-        }else if(a==b || b==c || a==c){
-            printf("TRIANGULO ISOSCELES\n");
-        }
-    }
-    else if(pow(a,2)<pow(b,2)+pow(c,2)){
-        printf("TRIANGULO ACUTANGULO\n");
-
-        if(a>=b+c){
-            printf("NAO FORMA TRIANGULO\n");
-        }else if(pow(a,2)==pow(b,2)+pow(c,2)){
-            printf("TRIANGULO RETANGULO\n");
-        }else if(pow(a,2)>pow(b,2)+pow(c,2)){
-            printf("TRIANGULO OBTUSANGULO\n");
-        }else if(a==b && b==c){
-            printf("TRIANGULO EQUILATERO\n");
-        }else if(a==b || b==c || a==c){
-            printf("TRIANGULO ISOSCELES\n");
-        }
-    }
-    else if(a==b && b==c){
-        printf("TRIANGULO EQUILATERO\n");
-
-        if(a>=b+c){
-            printf("NAO FORMA TRIANGULO\n");
-        }else if(pow(a,2)==pow(b,2)+pow(c,2)){
-            printf("TRIANGULO RETANGULO\n");
-        }else if(pow(a,2)>pow(b,2)+pow(c,2)){
-            printf("TRIANGULO OBTUSANGULO\n");
-        }else if(pow(a,2)<pow(b,2)+pow(c,2)){
-            printf("TRIANGULO ACUTANGULO\n");
-        }else if(a==b || b==c || a==c){
-            printf("TRIANGULO ISOSCELES\n");
-        }
-    }
-    else if(a==b || b==c || a==c){
-        printf("TRIANGULO ISOSCELES\n");
-
-        if(a>=b+c){
-            printf("NAO FORMA TRIANGULO\n");
-        }else if(pow(a,2)==pow(b,2)+pow(c,2)){
-            printf("TRIANGULO RETANGULO\n");
-        }else if(pow(a,2)>pow(b,2)+pow(c,2)){
-            printf("TRIANGULO OBTUSANGULO\n");
-        }else if(pow(a,2)<pow(b,2)+pow(c,2)){
-            printf("TRIANGULO ACUTANGULO\n");
-        }else if(a==b && b==c){
-            printf("TRIANGULO EQUILATERO\n");
-        }
-    }
     return 0;
 }
