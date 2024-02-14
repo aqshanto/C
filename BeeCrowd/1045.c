d68bd34adc8182668aba0b92dad12a367e9761ae
aqShanto <abdulquymshanto@gmail.com>
2024-02-14 12:40:57 +0600

    update 1045

--- BeeCrowd/1045.c
+++ BeeCrowd/1045.c
@@ -2,50 +2,53 @@
 #include<math.h>
 
 int main(){
-    double a,b,c,a1,b1,c1;
+    double a,b,c;
 
-    scanf("%lf %lf %lf", &a1 ,&b1 ,&c1);
-    if(a1>b1 && a1>c1){
-        a = a1;
-        if(b1>c1){
-            b =b1;
-            c =c1;
-        }else{
-            b =c1;
-            c =b1;
-        }
-    }
-    if(b1>a1 && b1>c1){
-        a = b1;
-        if(a1>c1){
-            b =a1;
-            c =c1;
-        }else{
-            b =c1;
-            c =a1;
-        }
-    }
-    if(c1>b1 && c1>a1){
-        a = c1;
-        if(b1>a1){
-            b =b1;
-            c =a1;
-        }else{
-            b =a1;
-            c =b1;
-        }
-    }
-    if(a>=b+c)
-        printf("NAO FORMA TRIANGULO\n");
-    if(pow(a,2)==pow(b,2)+pow(c,2))
-            printf("TRIANGULO RETANGULO\n");
-    if(pow(a,2)>pow(b,2)+pow(c,2))
-            printf("TRIANGULO OBTUSANGULO\n");
-    if(pow(a,2)<pow(b,2)+pow(c,2))
-            printf("TRIANGULO ACUTANGULO\n");
-    if(a==b && b==c)
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if(a >= (b+c) || b >= (a+c) || c >= (a+b))
+         printf("NAO FORMA TRIANGULO\n");
+    else if(a*a == (b*b + c*c) || b*b == (a*a + c*c) || c*c == (a*a + b*b))
+           printf("TRIANGULO RETANGULO\n");
+    else if(a*a > (b*b + c*c) || b*b > (a*a + c*c) || c*c > (a*a + b*b))
+           printf("TRIANGULO OBTUSANGULO\n");
+    else if(a*a < (b*b + c*c) || b*b < (a*a + c*c) || c*c < (a*a + b*b))
+           printf("TRIANGULO ACUTANGULO\n");
+    if(a == b && a == c)
             printf("TRIANGULO EQUILATERO\n");
-    if(a==b || b==c || a==c)
+    if((a == b && a != c)  || (a == c && a != b) || (b == c && b != a))
             printf("TRIANGULO ISOSCELES\n");
+
     return 0;
+    // double a,b,c,temp;
+
+    // scanf("%lf %lf %lf", &a,&b,&c);
+    // if(a<b){
+    //     temp =a;
+    //     a=b;
+    //     b= temp;
+    // }
+    // if(a<c){
+    //     temp =a;
+    //     a =c;
+    //     c =temp;
+    // }
+    // if(b<c){
+    //     temp =b;
+    //     b=c;
+    //     c =temp;
+    // }
+    // if(a>=b+c)
+    //     printf("NAO FORMA TRIANGULO\n");
+    // if(pow(a,2)==(pow(b,2)+pow(c,2)))
+    //         printf("TRIANGULO RETANGULO\n");
+    // if(pow(a,2)>(pow(b,2)+pow(c,2)))
+    //         printf("TRIANGULO OBTUSANGULO\n");
+    // if(pow(a,2)<(pow(b,2)+pow(c,2)))
+    //         printf("TRIANGULO ACUTANGULO\n");
+    // if(a==b && b==c)
+    //         printf("TRIANGULO EQUILATERO\n");
+    // if((a==b && a!=c) || (b==c && a!=c))
+    //         printf("TRIANGULO ISOSCELES\n");
+    // return 0;
 }
