448bc63a7f4d359d3875a26f05263bca96b3db3c
aqShanto <abdulquymshanto@gmail.com>
2024-02-15 23:03:14 +0600

    update 1078

--- BeeCrowd/1078.c
+++ BeeCrowd/1078.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d",&n);
     if(n>1 && n<1000){
         for (int i = 1; i <=10; i++){
-        printf("%d x %d = %d\n",i,n,(i*140));
+        printf("%d x %d = %d\n",i,n,(i*n));
     }
     }
     return 0;
