6ec02d79ded1d4f98d1d56066094111d7a6bee65
aqShanto <abdulquymshanto@gmail.com>
2024-02-20 00:47:16 +0600

    backup

--- BeeCrowd/2313.c
+++ BeeCrowd/2313.c
@@ -1,14 +1,43 @@
 #include<stdio.h>
 #include<math.h>
-
-int main(){
-    int a,b,c,x,y,z;
+int main()
+{
+    int a,b,c,d,e,f;
     scanf("%d %d %d",&a,&b,&c);
-        x=pow(a,2);
-        y=pow(b,2);
-        z=pow(c,2);
-        printf("%d\n",x);
-        printf("%d\n",y);
-        printf("%d\n",z);
+    d=pow(a,2);
+    e=pow(b,2);
+    f=pow(c,2);
+    if(a+b>c && b+c>a && a+c>b){
+        if(a==b && b==c && c==a){
+            printf("Valido-Equilatero\n");
+            if(f==(d)+(e) || d==(f)+(e) || e==(d)+(f)){
+                printf("Retangulo: S\n");
+            }
+            else{
+            printf("Retangulo: N\n");
+            }
+        }
+        else if(a==b || b==c || c==a){
+            printf("Valido-Isoceles\n");
+            if(f==(d)+(e) || d==(f)+(e) || e==(d)+(f)){
+                printf("Retangulo: S\n");
+            }
+            else{
+            printf("Retangulo: N\n");
+            }
+        }
+        else if(a!=b && b!=c && c!=a){
+            printf("Valido-Escaleno\n");
+            if(f==(d)+(e) || d==(f)+(e) || e==(d)+(f)){
+                printf("Retangulo: S\n");
+            }
+            else{
+            printf("Retangulo: N\n");
+            }
+        }
+    }
+    else{
+        printf("Invalido\n");
+    }
     return 0;
 }
