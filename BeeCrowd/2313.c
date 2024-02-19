0fa47f3b3c3017bb88993c076be4fc7ec476e082
aqShanto <abdulquymshanto@gmail.com>
2024-02-19 22:07:26 +0600

    solving 2313

---
+++ BeeCrowd/2313.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int a,b,c,x,y,z;
+    scanf("%d %d %d",&a,&b,&c);
+        x=pow(a,2);
+        y=pow(b,2);
+        z=pow(c,2);
+        printf("%d\n",x);
+        printf("%d\n",y);
+        printf("%d\n",z);
+    return 0;
+}
