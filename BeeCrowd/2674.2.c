83b42682f9a06a3fb5253303eee08a2a03be7844
aqShanto <abdulquymshanto@gmail.com>
2024-02-29 20:51:55 +0600

    solving 2674

--- BeeCrowd/2674.2.c
+++ BeeCrowd/2674.2.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
     int num,c=1;
     while(scanf("%d",&num)!=EOF){
@@ -36,5 +36,57 @@ int main(){
                 }
         }
     }
+    return 0;
+}
+*/
+#include<stdio.h>
+
+int main(){
+    int num,count,c=1,reminder;
+    while (scanf("%d",&num)!=EOF){
+        count=0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                count++;
+            }
+            if(count>2){
+                c=1;
+                break;
+            }
+            else if (count<=2){
+                c=2;
+            }
+        }
+        if(c==1){
+            printf("Nada\n");
+        }
+        else if(c==2){            
+            while(num!=0){
+                reminder=num%10;
+                count=0;
+                for(int i=1;i<=reminder;i++){
+                    if(reminder%i==0){
+                        count++;
+                    }
+                    if(count>2){
+                        printf("Primo\n");
+                        c=2;
+                        break;
+                    }
+                    else if (count<=2){
+                        c=3;
+                    }
+                }
+                num=num/10;
+                if(c==2){
+                    break;
+                }
+            }
+        }
+        if(c==3){
+            printf("Super\n");
+        }        
+    }
+    
     return 0;
 }
