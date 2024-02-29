b1ccc010699af0fbfa2f1162a99302b69b2e6c1d
aqShanto <abdulquymshanto@gmail.com>
2024-02-29 21:40:48 +0600

    solving 1768

--- BeeCrowd/1768.c
+++ BeeCrowd/1768.c
@@ -1,17 +1,32 @@
 #include<stdio.h>
 int main(){
-    // char c ='*';
-    // printf("%c\n",c);
-    // printf("%c\n",c);
-    // printf("%c\n",c);
-    int n;
+    int n,x=1;
     char star[100];
-    scanf("%d",&n);
-    for (int i = 0; i < n; i++)
-    {
-        star[i]='*';
+    while (scanf("%d",&n)!=EOF){
+        int height=(n+1)/2;
+        for (int i = 1; i<=height; i++){
+            int starCount  = (i*2)-1;
+            int spaceCount = n/2;
+            for (int j = i; j<=spaceCount; j++){
+                printf(" ");
+            }
+            for (int i = 1; i<=starCount; i++){
+                printf("*");
+            }
+            printf("\n");                       
+        }
+        for (int i = 1; i<=2; i++){
+            int starCount  = (i*2)-1;
+            int spaceCount = n/2;
+            for (int j = i; j<=spaceCount; j++){
+                printf(" ");
+            }
+            for (int i = 1; i<=starCount; i++){
+                printf("*");
+            }
+            printf("\n");                       
+        }
+        printf("\n");
     }
-    printf("%s",star);
-    
     return 0;
 }
