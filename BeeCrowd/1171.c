e795b9b3756157e411c30157ea55f2167c1c6059
aqShanto <abdulquymshanto@gmail.com>
2024-03-01 22:55:37 +0600

    solving 1318

---
+++ BeeCrowd/1171.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int X,arr[2010];
+    scanf("%d",&X);
+    for(int i=1;i<=X;i++){
+        scanf("%d",arr[i]);
+        for(int i=0;;i++){
+            if(arr[i])
+        }
+    }
+    return 0;
+}
--- BeeCrowd/2674.2.c
+++ BeeCrowd/2674.2.c
@@ -1,44 +1,3 @@
-/*#include<stdio.h>
-int main(){
-    int num,c=1;
-    while(scanf("%d",&num)!=EOF){
-        for(int i=1;i<=num;i++){
-            int count =0;
-            if(num%i==0){
-                count++;
-            }
-            if (count==2)
-            {
-                printf("Nada\n");
-            }
-            else{
-                c=2;
-            }
-        }
-        if(c==2){
-            while (num!=0){
-                    int reminder=num%10;
-                    for(int i=2;i<reminder;i++){
-                        if(reminder%i==0){
-                            c=1;
-                            break;
-                        }
-                    }
-                    if(c==1){
-                        break;
-                    }
-                    num=num/10;
-                }
-                if(c==2){
-                    printf("Super\n");
-                }else{
-                    printf("Primo\n");
-                }
-        }
-    }
-    return 0;
-}
-*/
 #include<stdio.h>
 
 int main(){
@@ -62,14 +21,12 @@ int main(){
         }
         else if(c==2){            
             while(num!=0){
-                reminder=num%10;
                 count=0;
-                for(int i=1;i<=reminder;i++){
-                    if(reminder%i==0){
+                for(int i=1;i<=num;i++){
+                    if(num%i==0){
                         count++;
                     }
                     if(count>2){
-                        printf("Primo\n");
                         c=2;
                         break;
                     }
@@ -82,6 +39,9 @@ int main(){
                     break;
                 }
             }
+        }
+        if(c==2){
+            printf("Primo\n");
         }
         if(c==3){
             printf("Super\n");
