3ac182ab65b0e353944fb5928daeea7d509e9fce
aqShanto <abdulquymshanto@gmail.com>
2024-04-04 23:10:19 +0600

    solving Assignment 1 of Phitron

--- HackerRank/Assignment 01/Shopping.c
+++ HackerRank/Assignment 01/Shopping.c
@@ -4,12 +4,12 @@ int main() {
     long long int a;
     scanf("%lld",&a);
     if(a>=1500){
-        printf("I will Buy Punjabi\n");
+        printf("I will buy Punjabi\n");
         printf("I will buy new shoes\n");
         printf("Alisa will buy new shoes\n");
     }
     else if(a>1000){
-        printf("I will Buy Punjabi\n");
+        printf("I will buy Punjabi\n");
     }
     else if(a<=1000){
         printf("Bad luck!\n");
