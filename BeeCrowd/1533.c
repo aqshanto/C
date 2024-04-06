4041bbc81ff885f4afd4d4936e836ccc80578dea
aqShanto <abdulquymshanto@gmail.com>
2024-04-06 22:14:48 +0600

    updating 1533

--- BeeCrowd/1533.c
+++ BeeCrowd/1533.c
@@ -1,19 +1,32 @@
 #include<stdio.h>
 
 int main(){
-    int N,position=0;
+    int N,i,j,position=0;
     while(N!=0){
         scanf("%d",&N);
+        if(N==0){
+            break;
+        }
         int V[N];
         for(int i=0;i<N;i++){
             scanf("%d",&V[i]);
         }
-        for(int i=0;i<N;i++){
-            if(V[i]==N){
-                position=i+1;
-                break;
-            }
+        for(int i=0;i<N-1;i++){
+            for(int j=i+1;j<N;j++){
+                if(V[i]<V[j]){
+                    if(i==1){
+                        int position=j;
+                        printf("%d\n",position);
+                    }
+                    int temp=V[i];
+                    V[i]=V[j];
+                    V[j]=temp;
+                }
+            }            
         }
+        // for(int i=0;i<N;i++){
+        //     printf("%d ",V[i]);
+        // }
         printf("%d\n",position);
     }
     return 0;
