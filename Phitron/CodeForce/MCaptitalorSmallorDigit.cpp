7ffe3c5da21eb1fe83be9c7983425d72374ae9e4
aqShanto <abdulquymshanto@gmail.com>
2024-04-07 02:01:01 +0600

    solving Capital small and digit

---
+++ Phitron/CodeForce/MCaptitalorSmallorDigit.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch<=57 && ch>=47){
+        printf("IS DIGIT\n");
+    }else if(ch>=65 && ch<=90){
+        printf("ALPHA\n");
+        printf("IS CAPITAL\n");
+    }else if(ch>=97 && ch<=122){
+        printf("ALPHA\n");
+        printf("IS SMALL\n");
+    }
+    return 0;
+}
