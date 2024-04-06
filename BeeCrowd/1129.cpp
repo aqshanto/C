96d05cc95c5c3d4edc9d02e647dca969cf461db9
aqShanto <abdulquymshanto@gmail.com>
2024-04-07 03:33:37 +0600

    solving 1129

--- BeeCrowd/1129.cpp
+++ BeeCrowd/1129.cpp
@@ -6,35 +6,38 @@ int main(){
         scanf("%d",&test);
         if(test==0)
             break;
-        int count =0;
-        for(int i=0;i<test;i++){
-            scanf("%d",&alternatives[i]);
-        }
-        for(int i=0;i<test;i++){
-            if(alternatives[i]<=127){
-                count++;
-                position=i;
+        int count;
+        for(int k=0;k<test;k++){
+            count=0,position=0;
+            for(int j=0;j<5;j++){
+                scanf("%d",&alternatives[j]);
             }
-            if(count<1){
+            for(int i=0;i<5;i++){
+                if(alternatives[i]<=127){
+                    count++;
+                    position=i;
+                    //printf("count-%d i-%d position-%d\n",count,i,position);
+                }
+            }
+            if(count>1 || count<1){
                 printf("*\n");
-                break;
             }
-        }
-        if(count==1){
-            if(position==0){
-                printf("A\n");
-            }
-            if(position==1){
-                printf("B\n");
-            }
-            if(position==2){
-                printf("C\n");
-            }
-            if(position==3){
-                printf("D\n");
-            }
-            if(position==4){
-                printf("E\n");
+            if(count==1){
+                if(position==0){
+                    printf("A\n");
+                }
+                if(position==1){
+                    printf("B\n");
+                }
+                if(position==2){
+                    printf("C\n");
+                }
+                if(position==3){
+                    printf("D\n");
+                }
+                if(position==4){
+                    printf("E\n");
+                }
             }
         }
     }
