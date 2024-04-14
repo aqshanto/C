1f904b4800aa5bef7fb0bf98b98fc87a606359ea
aqShanto <abdulquymshanto@gmail.com>
2024-04-14 18:14:38 +0600

    solving 2552

--- BeeCrowd/1267.cpp
+++ BeeCrowd/1267.cpp
@@ -1,34 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    int N,D,a[501][101],count;
+    int N,D,a[501][101],count,i,j;
     while (N!=0 && D!=0)
     {
         scanf("%d %d",&N,&D);
         if(N==0 && D==0){
             break;
         }
-        count=0;
-        for (int i = 0; i < D; i++)
-        {
-            for (int j = 0; j < N; j++)
-            {
+        for (i = 0; i < N; i++){
+            for (j = 0; j < D; j++){
                 scanf("%d",&a[i][j]);
             }
         }
-        // for (int i = 0; i < D; i++)
-        // {
-        //     for (int j = 0; j < N; j++)
-        //     {
-        //         printf("%d ",a[i][j]);
-        //     }
-        //     printf("\n");
-        // }
-        for (int j = 0; j < N; j++)
-        {
+        for (i = 0; i < D; i++){
             count=0;
-            for (int i = 0; i < D; i++)
-            {
+            for (j = 0; j < N; j++){
                 if(a[i][j]==1){
                     count++;
                 }
---
+++ BeeCrowd/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+        // for (i = 0; i <=n+1; i++){
+        //     for(j=0;j<=m+1;j++){
+        //         printf("%d",board[i][j]);
+        //     }
+        //     printf("\n");
+        // }
