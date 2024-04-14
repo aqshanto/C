c6172afc6ba6e68cd916e27df54fe4cd00746744
aqShanto <abdulquymshanto@gmail.com>
2024-04-14 19:13:15 +0600

    solving 1848

--- BeeCrowd/1848.cpp
+++ BeeCrowd/1848.cpp
@@ -1,37 +1,34 @@
 #include<stdio.h>
 #include<string.h>
-#include<math.h>
 
 int main(){
-    int screamCount=0,ticketNum=0,count[3];
-    char sign[10];
-    for(int i=0;;i++){
-        fgets(sign,10,stdin);
-        if(strcmp(sign,"caw caw")==0){
-            screamCount++;
-        }else{
-            for(int i=0;i<3;i++){
-                for(int j=2;j>=0;j--){
-                    if(sign[i]=='*'){
-                        count[j]=1;
-                    }
-                    else{
-                        count[j]=0;
-                    }
-                    printf("%d ",count[j]);
-                }
-            }
-            int m=2;
-            for(int k=0;k<3;k++){
-                ticketNum=ticketNum+(count[k]*m);
-                m--;
-            }
-            printf("%d\n",ticketNum);
+    char eye[10];
+    int  sum=0,count=0;
+    while(count!=3){
+        fgets(eye,sizeof(eye),stdin);
+        if(strcmp(eye,"caw caw\n")==0){
+            printf("%d\n",sum);
+            sum=0;
+            count++;
         }
-        if(screamCount==3){
+        if(strcmp(eye,"--*\n")==0){
+            sum+=1;
+        }if(strcmp(eye,"-*-\n")==0){
+            sum+=2;
+        }if(strcmp(eye,"-**\n")==0){
+            sum+=3;
+        }if(strcmp(eye,"*--\n")==0){
+            sum+=4;
+        }if(strcmp(eye,"*-*\n")==0){
+            sum+=5;
+        }if(strcmp(eye,"**-\n")==0){
+            sum+=6;
+        }if(strcmp(eye,"***\n")==0){
+            sum+=7;
+        }
+        if(count==3){
             break;
         }
-
     }
     return 0;
 }
