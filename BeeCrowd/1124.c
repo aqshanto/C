68c806d7949fc29b23ec484683b268ef5782e650
aqShanto <abdulquymshanto@gmail.com>
2024-04-30 03:01:44 +0600

    update

--- BeeCrowd/1124.c
+++ BeeCrowd/1124.c
@@ -8,7 +8,7 @@ int main(){
         if (l==0&&c==0&&r1==0&&r2==0){
             break;
         }
-        printf("l = %lf c = %lf r1 = %lf r2 = %lf\n",l,c,r1,r2);
+        //printf("l = %lf c = %lf r1 = %lf r2 = %lf\n",l,c,r1,r2);
         double cir1extarea = ((sqrt(2)*(2*r1))-(2*r1))/2;
         double cir2extarea = ((sqrt(2)*(2*r2))-(2*r2))/2;
         double diagonal = sqrt((l*l)+(c*c));
@@ -16,7 +16,7 @@ int main(){
         double area1= cir1extarea+(2*r1);
         double area2= cir2extarea+(2*r2);
         double total=area1+area2;
-        printf("cir1extarea = %lf cir2extarea = %lf area1 = %lf area2 = %lf diagonal = %lf total = %lf\n",cir1extarea,cir2extarea,area1,area2,diagonal,area1+area2);
+        //printf("cir1extarea = %lf cir2extarea = %lf area1 = %lf area2 = %lf diagonal = %lf total = %lf\n",cir1extarea,cir2extarea,area1,area2,diagonal,area1+area2);
         if (diagonal>=total){
             printf("S\n");
         }
