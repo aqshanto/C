11f6074495537e227b71726f931656b0655b3d97
aqShanto <abdulquymshanto@gmail.com>
2024-05-10 01:28:12 +0600

    update

--- BeeCrowd/1160.cpp
+++ BeeCrowd/1160.cpp
@@ -8,11 +8,17 @@ int main(){
     double rA,rB;
     for(int i=0;i<test;i++){
         scanf("%d %d %lf %lf",&A,&B,&rA,&rB);
-        double dif=(rA-rB)/100;
-        double year=abs(B-A+1)/(A*dif);
+        int year=0;
+        while(A<=B){
+            A=A+A*rA/100;
+            B=B+B*rB/100;
+            year++;
+            if(year>100){
+                break;
+            }
+        }
         if(year<=100){
-            int result=floor(year);
-            printf("%d anos\n",result);
+            printf("%d anos\n",year);
         }
         else{
             printf("Mais de 1 seculo.\n");
---
+++ BeeCrowd/1532.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int n,v,lap;
+    while(1){
+        scanf("%d %d",&n,&lap);
+        v=lap;
+        int check=0;
+        if(n==0 && v==0){
+            break;
+        }
+        int start=0,count=0,i=1;
+        while(start<=n){
+            start+=v;
+            count++;
+            if(v==1 && count==1){
+                v=lap-i;
+                i++;
+                if(v==0){
+                    break;
+                }
+            }
+            if(count==v){
+                v--;
+                count=0;
+            }
+            if(start==n){
+                check=1;
+                break;
+            }
+        }
+        if(check==1)printf("possivel\n");
+        else printf("impossivel\n");
+    }
+}
